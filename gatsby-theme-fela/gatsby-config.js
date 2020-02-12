fa60656e4eda77ff23eb563eafa433a2c02074d8
Filip Kubik <filip.kubik@ackee.cz>
2020-02-12 15:49:23 +0100

    :recycle: Remove pages form theme
    Remove pages form theme

--- gatsby-theme-fela/gatsby-config.js
+++ gatsby-theme-fela/gatsby-config.js
@@ -1,16 +1,15 @@
 module.exports = () => ({
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    "gatsby-plugin-fela",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
+    // `gatsby-plugin-sharp`,
+    // `gatsby-transformer-sharp`,
+    "gatsby-plugin-fela"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- gatsby-theme-fela/src/components/Layout.js
+++ gatsby-theme-fela/src/components/Layout.js
@@ -1,13 +1,5 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import { FelaComponent } from "react-fela"
 
 import FelaProvider from "./FelaProvider"
@@ -15,19 +7,9 @@ import Header from "./Header"
 import { wrapper } from "./Layout.style"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <FelaProvider>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header />
       <FelaComponent style={wrapper}>
         <main>{children}</main>
         <footer>
