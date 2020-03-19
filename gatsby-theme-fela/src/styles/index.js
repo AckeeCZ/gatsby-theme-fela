90c379559de3bf42cfab6b72e7ed17f37eb6a308
filip-dot-cube <filcek23@gmail.com>
2020-03-19 15:41:17 +0100

    🚨 ignore no-webpack-loader
    ignore no-webpack-loader

--- gatsby-theme-fela/src/styles/index.js
+++ gatsby-theme-fela/src/styles/index.js
@@ -1,6 +1,8 @@
-import layout from "!css-loader!./layout.css"
+/* eslint-disable import/no-webpack-loader-syntax, import/no-unresolved*/
+import layout from '!css-loader!./layout.css';
 import resets from '!css-loader!./resets.css';
+/* eslint-enable import/no-webpack-loader-syntax, import/no-unresolved*/
 
-export { default as theme } from "./theme"
+export { default as theme } from './theme';
 
-export default [layout, resets]
+export default [layout, resets];
--- site/src/gatsby-theme-fela/styles/index.js
+++ site/src/gatsby-theme-fela/styles/index.js
@@ -1,6 +1,8 @@
-import layout from "!css-loader!./layout.css"
-import customStaticStyles from "!css-loader!./customStaticStyles.css"
+/* eslint-disable import/no-webpack-loader-syntax, import/no-unresolved*/
+import layout from '!css-loader!./layout.css';
+import customStaticStyles from '!css-loader!./customStaticStyles.css';
+/* eslint-enable import/no-webpack-loader-syntax, import/no-unresolved*/
 
-export { default as theme } from "./theme"
+export { default as theme } from './theme';
 
-export default [layout, customStaticStyles]
+export default [layout, customStaticStyles];
