ac15a465c16ee4241ac0b60c022a776e4dc27cf9
filip-dot-cube <filcek23@gmail.com>
2020-03-24 17:04:46 +0100

    🔧 Add babel script and preset
    Add babel script and preset

--- gatsby-theme-fela/src/components/FelaKeyFrame.jsx
+++ gatsby-theme-fela/src/components/FelaKeyFrame.jsx
@@ -1,19 +1,21 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { useFela } from "react-fela"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useFela } from 'react-fela';
 
 function FelaKeyframe({ children, keyframe, ...props }) {
-  const { renderer, theme } = useFela()
-  const animationName = React.useMemo(
-    () => renderer.renderKeyframe(keyframe, { ...props, theme }),
-    [keyframe, renderer, theme, props]
-  )
-  return children(animationName)
+  const { renderer, theme } = useFela();
+  const animationName = React.useMemo(() => renderer.renderKeyframe(keyframe, { ...props, theme }), [
+    keyframe,
+    renderer,
+    theme,
+    props,
+  ]);
+  return children(animationName);
 }
 
 FelaKeyframe.propTypes = {
   children: PropTypes.func.isRequired,
   keyframe: PropTypes.func.isRequired,
-}
+};
 
-export default FelaKeyframe
+export default FelaKeyframe;
