3f2328730a8a11354056a8219ec32345d1e790ad
Lukas Horak <horaklukas@centrum.cz>
2022-07-20 16:32:53 +0200

    ♻️ Change gatsby apis used in ssr for  styles server render

--- gatsby-theme-fela/gatsby-ssr.js
+++ gatsby-theme-fela/gatsby-ssr.js
@@ -1,35 +1,48 @@
 import React from 'react';
-import { renderToString } from 'react-dom/server';
 import { RendererProvider, ThemeProvider } from 'react-fela';
 import { createRenderer } from 'fela';
 import { renderToSheetList } from 'fela-dom';
 
-import { rendererConfig } from './src/config';
-
+import { rendererConfig, staticCSS, fonts } from './src/config';
 import { theme } from './src/styles';
+import { applyStaticCSS, applyFonts } from './src/utilities';
 
-export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
+const rendererByPathname = new Map();
+
+// eslint-disable-next-line react/prop-types,react/display-name
+export const wrapRootElement = ({ element, pathname }) => {
   const renderer = createRenderer(rendererConfig);
 
-  const bodyHTML = renderToString(
+  applyStaticCSS(renderer, staticCSS);
+  applyFonts(renderer, fonts);
+
+  rendererByPathname.set(pathname, renderer);
+
+  return (
     // eslint-disable-next-line react/jsx-filename-extension
     <RendererProvider renderer={renderer}>
-      <ThemeProvider theme={theme}>{bodyComponent}</ThemeProvider>
-    </RendererProvider>,
+      <ThemeProvider theme={theme}>{element}</ThemeProvider>
+    </RendererProvider>
   );
-  const sheetList = renderToSheetList(renderer);
+};
 
-  const elements = sheetList.map(({ type, css, media, support, rehydration }) => (
-    <style
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: css }}
-      data-fela-type={type}
-      data-fela-support={support}
-      data-fela-rehydration={rehydration}
-      key={`${type}-${media}`}
-      media={media}
-    />
-  ));
-  replaceBodyHTMLString(bodyHTML);
-  setHeadComponents(elements);
+export const onRenderBody = ({ setHeadComponents, pathname }) => {
+  const renderer = rendererByPathname.get(pathname);
+  if (renderer) {
+    const sheetList = renderToSheetList(renderer);
+    const headElements = sheetList.map(({ type, css, media, support, rehydration }) => (
+      <style
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: css }}
+        data-fela-type={type}
+        data-fela-support={support}
+        data-fela-rehydration={rehydration}
+        key={`${type}-${media}`}
+        media={media}
+      />
+    ));
+
+    setHeadComponents(headElements);
+    rendererByPathname.delete(pathname);
+  }
 };
